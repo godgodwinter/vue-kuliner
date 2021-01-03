652284f8b4a5c7ce26db7b7fe5fe036a735465a0
godgodwinter <kakadz.pro@gmail.com>
2021-01-03 16:24:48 +0700

    atur navbar dan home

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// start bootstrap
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+// end boostrap
+
 Vue.config.productionTip = false
 
 new Vue({
